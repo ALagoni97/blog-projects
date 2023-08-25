9774e300119da353733af4a867684d334b119fc6
Lagoni <andreastlagoni@gmail.com>
2023-08-25 15:18:54 +0200

    Update UserResolver.ts

--- graphql-backend-prisma/src/resolvers/UserResolver.ts
+++ graphql-backend-prisma/src/resolvers/UserResolver.ts
@@ -1,64 +1,65 @@
 import { GQLResolvers } from "../__generated__/resolvers-types";
-
-// export const UserResolver: GQLResolvers = {
-//   Query: {
-//     users: async (_, { pagination }, context) => {
-//       const users = await context.database.user.findMany({
-//         take: pagination.perPage,
-//       });
-//       return users;
-//     },
-//   },
-//   User: {
-//     posts: async (parent, args, context) => {
-//       const post = await context.database.user
-//         .findUnique({
-//           where: {
-//             userId: parent.userId,
-//           },
-//         })
-//         .Posts();
-//       return post;
-//     },
-//   },
-//   Post: {
-//     comments: async (parent, args, context) => {
-//       const comments = await context.database.post
-//         .findUnique({
-//           where: {
-//             postId: parent.postId,
-//           },
-//         })
-//         .Comments();
-//       return comments;
-//     },
-//   },
-// };
-
-/* 
-Current include way
-*/
+import { databaseConnection } from "../context/Database";
 
 export const UserResolver: GQLResolvers = {
   Query: {
     users: async (_, { pagination }, context) => {
       const users = await context.database.user.findMany({
-        include: {
-          Posts: {
-            include: {
-              Comments: true,
-            },
-          },
-        },
         take: pagination.perPage,
       });
-      return users.map((user) => ({
-        ...user,
-        posts: user.Posts.map((post) => ({
-          ...post,
-          comments: post.Comments,
-        })),
-      }));
+      return users;
+    },
+  },
+  User: {
+    posts: async (parent, args, context) => {
+      const post = await context.database.user
+        .findUnique({
+          where: {
+            userId: parent.userId,
+          },
+        })
+        .Posts();
+      return post;
+    },
+  },
+  Post: {
+    comments: async (parent, args, context) => {
+      const comments = await context.database.post
+        .findUnique({
+          where: {
+            postId: parent.postId,
+          },
+        })
+        .Comments({ take: 2 });
+      return comments;
     },
   },
 };
+
+/* 
+Current include way
+*/
+
+// export const UserResolver: GQLResolvers = {
+//   Query: {
+//     users: async (_, { pagination }, context) => {
+//       const users = await context.database.user.findMany({
+//         include: {
+//           Posts: {
+//             include: {
+//               Comments: true,
+//             },
+//           },
+//         },
+//         take: pagination.perPage,
+//       });
+//       return users.map((user) => ({
+//         ...user,
+//         posts: user.Posts.map((post) => ({
+//           ...post,
+//           comments: post.Comments,
+//         })),
+//       }));
+//     },
+//   },
+// };
