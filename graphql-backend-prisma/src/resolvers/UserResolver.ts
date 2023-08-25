99b6f93ed8f168a08a7d3ac292736351d58c33ee
Lagoni <andreastlagoni@gmail.com>
2023-08-25 15:02:11 +0200

    fix resolver issue

--- graphql-backend-prisma/src/resolvers/UserResolver.ts
+++ graphql-backend-prisma/src/resolvers/UserResolver.ts
@@ -1,22 +1,64 @@
 import { GQLResolvers } from "../__generated__/resolvers-types";
 
+// export const UserResolver: GQLResolvers = {
+//   Query: {
+//     users: async (_, { pagination }, context) => {
+//       const users = await context.database.user.findMany({
+//         take: pagination.perPage,
+//       });
+//       return users;
+//     },
+//   },
+//   User: {
+//     posts: async (parent, args, context) => {
+//       const post = await context.database.user
+//         .findUnique({
+//           where: {
+//             userId: parent.userId,
+//           },
+//         })
+//         .Posts();
+//       return post;
+//     },
+//   },
+//   Post: {
+//     comments: async (parent, args, context) => {
+//       const comments = await context.database.post
+//         .findUnique({
+//           where: {
+//             postId: parent.postId,
+//           },
+//         })
+//         .Comments();
+//       return comments;
+//     },
+//   },
+// };
+
+/* 
+Current include way
+*/
+
 export const UserResolver: GQLResolvers = {
   Query: {
-    users: async (_, args, context) => {
-      const users = await context.database.user.findMany();
-      return users;
-    },
-  },
-  User: {
-    posts: async (parent, args, context) => {
-      const post = context.database.user
-        .findUnique({
-          where: {
-            userId: parent.userId,
+    users: async (_, { pagination }, context) => {
+      const users = await context.database.user.findMany({
+        include: {
+          Posts: {
+            include: {
+              Comments: true,
+            },
           },
-        })
-        .Posts();
-      return post;
+        },
+        take: pagination.perPage,
+      });
+      return users.map((user) => ({
+        ...user,
+        posts: user.Posts.map((post) => ({
+          ...post,
+          comments: post.Comments,
+        })),
+      }));
     },
   },
 };
